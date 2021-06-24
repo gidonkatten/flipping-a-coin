24ff331ef9e5decabc8ebe76dc65306e507c9da3
Gidon <gidonkatten@gmail.com>
2021-06-24 16:24:21 +0100

    Coin flip implementation - untested

--- contracts/contract_account.py
+++ contracts/contract_account.py
@@ -4,12 +4,22 @@ from pyteal import *
 
 def contract_account(app_id):
 
+    rekey_check = Txn.rekey_to() == Global.zero_address()
+    fee_check = Txn.fee() <= Int(1000)
+    close_remainder_to_check = Txn.close_remainder_to() == Global.zero_address()
     linked_with_app_call = And(
         Gtxn[0].type_enum() == TxnType.ApplicationCall,
         Gtxn[0].application_id() == Int(app_id)
     )
 
-    return Int(1)
+    return Seq([
+        Assert(Txn.group_index() == Int(1)),
+        Assert(rekey_check),
+        Assert(fee_check),
+        Assert(close_remainder_to_check),
+        Assert(linked_with_app_call),
+        Int(1)
+    ])
 
 
 if __name__ == "__main__":
--- contracts/stateful.py
+++ contracts/stateful.py
@@ -2,31 +2,113 @@ from pyteal import *
 
 
 def stateful():
-
-    linked_with_escrow = Gtxn[1].sender() == App.globalGet(Bytes('escrow'))
+    # HEADS is non-zero - TAILS is zero
 
     # set escrow address
+    escrow = App.globalGetEx(Int(0), Bytes('escrow'))
     on_set_escrow = Seq([
+        Assert(Global.group_size() == Int(1)),
         Assert(Txn.sender() == Global.creator_address()),
+        escrow,
+        Assert(Not(escrow.hasValue())),
         App.globalPut(Bytes('escrow'), Txn.application_args[1]),
         Int(1)
     ])
 
     # flip coin
-    # amount bet, hash of heads / tails with nonce
-    on_flip = Int(1)
+    on_flip = Seq([
+        Assert(Global.group_size() == Int(2)),
+        # check payment
+        Assert(Gtxn[1].type_enum() == TxnType.Payment),
+        Assert(Gtxn[1].sender() == Txn.sender()),
+        Assert(Gtxn[1].receiver() == App.globalGet(Bytes('escrow'))),
+        # store flip
+        App.globalPut(Bytes('bet'), Gtxn[1].amount()),
+        App.globalPut(Bytes('secret'), Txn.application_args[1]),
+        App.globalPut(Bytes('flipper'), Txn.sender()),
+        Int(1)
+    ])
 
     # guess
-    # heads / tails
-    on_guess = Int(1)
+    bet = App.globalGetEx(Int(0), Bytes('bet'))
+    guess = App.globalGetEx(Int(0), Bytes('guess'))
+    one_day = Int(86400)
+    on_guess = Seq([
+        Assert(Global.group_size() == Int(2)),
+        # check there is an available bet
+        bet,
+        Assert(bet.hasValue()),
+        guess,
+        Assert(Not(guess.hasValue())),
+        # check payment
+        Assert(Gtxn[1].type_enum() == TxnType.Payment),
+        Assert(Gtxn[1].sender() == Txn.sender()),
+        Assert(Gtxn[1].receiver() == App.globalGet(Bytes('escrow'))),
+        Assert(Gtxn[1].amount() == bet.value()),
+        # store guess
+        App.globalPut(Bytes('guess'), Btoi(Txn.application_args[1]) >= Int(1)),
+        App.globalPut(Bytes('guesser'), Txn.sender()),
+        App.globalPut(Bytes('expiry'), Global.latest_timestamp() + one_day),
+        Int(1)
+    ])
 
     # reveal
     # heads / tail, nonce
-    on_reveal = Int(1)
+    revealed = Btoi(Txn.application_args[1]) >= Int(1)
+    nonce = Txn.application_args[1]
+    preimage = Concat(Substring(Bytes('01'), revealed, revealed + Int(1)), nonce)
+    correct = Eq(App.globalGet(Bytes('secret')), Sha256(preimage))
+    on_reveal = Seq([
+        Assert(Global.group_size() == Int(1)),
+        Assert(correct),
+        App.globalPut(Bytes('result'), revealed),
+        Int(1)
+    ])
 
     # claim
-    # winner can claim - if didn't reveal or opted out of application then guesser wins
-    on_claim = Int(1)
+    result = App.globalGetEx(Int(0), Bytes('result'))
+    on_claim = Seq([
+        Assert(Global.group_size() == Int(2)),
+        # check there was a bet
+        bet,
+        Assert(bet.hasValue()),
+        guess,
+        Assert(guess.hasValue()),
+        # check winner
+        result,
+        If(
+            Not(result.hasValue()),
+            # if no reveal can claim if guesser and past expiry
+            Seq([
+                Assert(Txn.sender() == App.globalGet(Bytes('guesser'))),
+                Assert(Global.latest_timestamp() >= App.globalGet(Bytes('expiry')))
+            ]),
+            # if have revealed then verify if won
+            Cond(
+                [
+                    Txn.sender() == App.globalGet(Bytes('flipper')),
+                    Assert(result.value() != App.globalGet(Bytes('guess')))
+                ],
+                [
+                    Txn.sender() == App.globalGet(Bytes('guesser')),
+                    Assert(result.value() == App.globalGet(Bytes('guess')))
+                ],
+            )
+        ),
+        # check payment
+        Assert(Gtxn[1].type_enum() == TxnType.Payment),
+        Assert(Gtxn[1].sender() == App.globalGet(Bytes('escrow'))),
+        Assert(Gtxn[1].receiver() == App.globalGet(Bytes('escrow'))),
+        Assert(Gtxn[1].amount() == (bet.value() * Int(2))),
+        # clean up global state for next coin flip
+        App.globalDel(Bytes('bet')),
+        App.globalDel(Bytes('secret')),
+        App.globalDel(Bytes('flipper')),
+        App.globalDel(Bytes('guess')),
+        App.globalDel(Bytes('guesser')),
+        App.globalDel(Bytes('expiry')),
+        Int(1)
+    ])
 
     program = Cond(
         [Txn.on_completion() == OnComplete.DeleteApplication, Int(0)],
